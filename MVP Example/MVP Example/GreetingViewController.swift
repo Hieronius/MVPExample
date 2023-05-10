7572e658ed7876b836112cc37a57a7f554225040
Arsenty Halimovsky <ghost12@yandex.ru>
2023-05-10 15:51:12 +0300

    Pass Model and Presenter to VC

--- MVP Example/MVP Example/GreetingViewController.swift
+++ MVP Example/MVP Example/GreetingViewController.swift
@@ -19,17 +19,29 @@ class GreetingViewController: UIViewController {
     
     // our label from Storyboard
     @IBOutlet var greetingLabel: UILabel!
+    
+    // View or ViewCOntroller "own's" Presenter
+    private var presenter: GreetingPresenterProtocol!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // 1. build instance of our Model
+        let person = Person(name: "Tim", surname: "Cook")
+        
+        
+        // 2. build instance of Presenter with properties
+        presenter = GreetingPresenter(view: self, person: person)
+        
     }
     
     // MARK: - IBActions
     
     // our button from Storyboard
     @IBAction func showGreetingPressed() {
-        
+        // 3. call method to change label 
+        presenter.showGreating()
     }
 
 }
