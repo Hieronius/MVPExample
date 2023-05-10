e270437cd7ef9773ea6a90c5268ddccd9efbcb02
Arsenty Halimovsky <ghost12@yandex.ru>
2023-05-10 15:31:33 +0300

    Define VC and add Model

---
+++ MVP Example/MVP Example/GreetingViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  MVP Example
+//
+//  Created by Арсентий Халимовский on 10.05.2023.
+//
+
+import UIKit
+
+// Protocol as public interface
+protocol GreetingViewProtocol {
+    func setGreeting(_ greeting: String)
+}
+
+// our "Passive View"
+class GreetingViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    
+    // our label from Storyboard
+    @IBOutlet var greetingLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - IBActions
+    
+    // our button from Storyboard
+    @IBAction func showGreetingPressed() {
+        
+    }
+
+}
+
+// MARK: - Extensions
+
+extension GreetingViewController: GreetingViewProtocol {
+    func setGreeting(_ greeting: String) {
+        // pass actual greeting to the label
+        greetingLabel.text = greeting
+    }
+    
+
+}
+
---
+++ MVP Example/MVP Example/Model.swift
@@ -0,0 +1,14 @@
+//
+//  Model.swift
+//  MVP Example
+//
+//  Created by Арсентий Халимовский on 10.05.2023.
+//
+
+import Foundation
+
+// Model - UserData
+struct Person {
+    let name: String
+    let surname: String
+}
